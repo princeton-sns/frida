f320d7c439f8dca12ad0160075e598f959c15063
Christopher Branner-Augmon <chris@dynamic-oit-vapornet100-10-8-144-247.princeton.edu>
2022-11-08 13:22:06 -0500

    Committing to pull changes in

--- apps/iot/lightswitch/client/src/App.vue
+++ apps/iot/lightswitch/client/src/App.vue
@@ -1,17 +1,41 @@
+/* eslint-disable no-unused-vars */
+/* eslint-enable no-unused-vars */
+<template>
+  <div class="add-friend">
+      <div>
+        <input v-model="friend" placeholder="device id key" />
+      </div>
+      <button @click="addFriend">Add Friend</button>
+  </div>
+  <div class="Home">
+    <input type="checkbox" v-model="checked" id="box" v-on:change="foo"/>
+    <label for="box">{{ checked }}</label>
+    <!-- <p>{{ pubkey }}</p>-->
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
       checked: false,
       count: 0,
-      pubkey: null
+      pubkey: null,
+      friend: null
     }
   },
   methods: {
   	foo() {
       //alert(pubkey)
-		}
-  } 
+		},
+  
+    addFriend(event) {
+      console.log(event);
+      (async () => {
+      await frida.addContact(this.friend);
+      })();
+      }
+    }
 }
 import * as frida from "../../../../../core/client";
 let serverIP = "sns26.cs.princeton.edu";
@@ -20,15 +44,9 @@ const skeletonPrefix = "skeletonData";
 (async () => {
   await frida.init(serverIP, serverPort, {storagePrefixes: [skeletonPrefix],});
   frida.createDevice(null, null);
-})();
-/* eslint-disable no-unused-vars */
-/* eslint-enable no-unused-vars */
-</script>
+  console.log(frida.getIdkey());
+  //await frida.setData(skeletonPrefix, "key", "value2");
+  console.log(await frida.getData(skeletonPrefix, "key"));
 
-<template>
-  <div class="Home">
-    <input type="checkbox" v-model="checked" id="box" v-on:change="foo"/>
-    <label for="box">{{ checked }}</label>
-    <!-- <p>{{ pubkey }}</p>-->
-  </div>
-</template>
+})();
+</script>

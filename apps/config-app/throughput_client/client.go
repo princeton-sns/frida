69da636741db3af1d627072b12fdc56448fa7e95
leochanj105 <leocjy105@gmail.com>
2022-12-02 01:33:58 -0500

    added go server benchmark app

---
+++ apps/config-app/throughput_client/client.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"fmt"
+	"github.com/r3labs/sse/v2"
+	"encoding/json"
+	"net/http"
+	"bytes"
+	"time"
+	"os"
+)
+
+type OutgoingMessage struct {
+	DeviceId string `json:"deviceId"`
+	Payload  interface{} `json:"payload"`
+}
+
+type IncomingMessage struct {
+	Sender   string `json:"sender"`
+	Payload  interface{} `json:"encPayload"`
+	SeqID    uint64 `json:"seqID"`
+}
+
+
+type Batch struct {
+	Batch [] OutgoingMessage `json:"batch"`
+}
+
+type BodyWithCseqID struct {
+	Body interface{} `json:"body"`
+	CseqID uint64 `json:"cseqID"`
+}
+
+// type NeedsOneTimeKeyEvent struct {
+// 	DeviceId string `json:"deviceId"`
+// 	Needs    uint `json:"needs"`
+// }
+
+const MAX_ROUTINES = 100
+
+var deviceId string;
+
+var serverAddr string = "http://localhost:8080";
+
+var msgSize = 32;
+
+var msgContent []byte;
+
+var recvCount uint64 = 0;
+
+var duration int64 = 10;
+
+var keepout int64 = 1;
+
+var startTime int64;
+
+func req(reqType string, jsonStr []byte, path string){
+	req, _ := http.NewRequest(reqType, serverAddr + path, bytes.NewBuffer(jsonStr))
+	req.Header = http.Header{
+		"Content-Type": {"application/json"},
+		"Authorization": {"Bearer " + deviceId},
+	}
+	_, err := (&http.Client{}).Do(req)
+	// defer resp.Body.Close()
+
+	if err != nil {
+        panic(err)
+    }
+}
+
+func sendTo(ids []string, cseqID uint64){
+	batch := new(Batch)
+	for _, id := range ids{
+		body := BodyWithCseqID{msgContent, cseqID}
+		msg := OutgoingMessage{id, body}
+		batch.Batch = append(batch.Batch, msg)
+	}
+	b, _ := json.Marshal(batch)
+	req("POST", b, "/message")
+}
+
+func delete(seqID uint64){
+	delMsg := fmt.Sprintf(`{"seqID" : %v }`, seqID)
+	req("DELETE", []byte(delMsg), "/self/messages")
+}
+
+func now() int64 {
+    return time.Now().UnixNano() / int64(time.Microsecond)
+}
+
+func main() {
+	deviceId = os.Args[1]
+	client := sse.NewClient(serverAddr + "/events")
+	client.Headers["Authorization"] = "Bearer " + deviceId
+
+	msgContent = make([]byte, msgSize)
+
+	finish := make(chan bool)
+	
+	go client.Subscribe("msg", func(msg *sse.Event) {
+		msgType := string([]byte(msg.Event))
+		
+		// if(msgType == "otkey"){
+			// var msgContent NeedsOneTimeKeyEvent
+			// json.Unmarshal([]byte(msg.Data), &msgContent)
+			// fmt.Println(msgContent.DeviceId)
+		// }
+
+		if(msgType == "msg"){
+			// fmt.Println(string([]byte(msg.Data)))
+			var msgContent IncomingMessage
+			json.Unmarshal([]byte(msg.Data), &msgContent)
+			delete(msgContent.SeqID)
+			timePassed := now() - startTime
+			if(timePassed >= keepout * 1000000 && timePassed <= (duration - keepout) * 1000000){
+				recvCount += 1
+				// cseqID := msgContent.Payload.(map[string] interface{})["cseqID"]
+				// fmt.Printf("recv time[%v]: %v\n", cseqID, now())
+			}
+		}
+
+	})
+	
+
+	listToSend := []string{deviceId}
+
+	var i uint64
+
+	sem := make(chan int, MAX_ROUTINES)
+
+	startTime = now()
+	for i = 0; (now() - startTime <= duration * 1000000); i++ {
+		sem <- 1
+		go func(id uint64){
+			// fmt.Printf("send time[%v]: %v\n", id, now())
+			sendTo(listToSend, id)
+			<-sem
+		}(i)
+	}
+	fmt.Printf("%v : %v", deviceId, recvCount)
+	<-finish
+}

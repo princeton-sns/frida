8219658d9ed0211cc242f77d03c72f05193a9f72
leochanj105 <leocjy105@gmail.com>
2022-12-02 22:43:31 -0500

    faster bench clients

--- experiments/throughput_bench/client.go
+++ experiments/throughput_bench/client.go
@@ -32,14 +32,9 @@ type BodyWithCseqID struct {
 	CseqID uint64 `json:"cseqID"`
 }
 
-// type NeedsOneTimeKeyEvent struct {
-// 	DeviceId string `json:"deviceId"`
-// 	Needs    uint `json:"needs"`
-// }
-
 const MAX_ROUTINES_SEND = 2
 
-// const MAX_ROUTINES_DELETE = 2
+const POST_PATH = "/message"
 
 var deviceId string;
 
@@ -48,6 +43,10 @@ var serverAddr string = "http://localhost:8080";
 var msgSize int64;
 
 var msgContent []byte;
+
+var msgBatch []byte;
+
+// var msgPostRequest *http.Request;
 
 var recvCount uint64 = 0;
 
@@ -64,11 +63,9 @@ var httpClient *http.Client;
 var numHead uint64;
 var numTail uint64;
 
-// var semDelete make(chan bool MAX_ROUTINES_DELETE);
-
 func req(reqType string, jsonStr []byte, path string) (*http.Response){
 	req, _ := http.NewRequest(reqType, serverAddr + path, bytes.NewBuffer(jsonStr))
-	req.Close = true
+	// req.Close = true
 	req.Header = http.Header{
 		"Content-Type": {"application/json"},
 		"Authorization": {"Bearer " + deviceId},
@@ -81,17 +78,17 @@ func req(reqType string, jsonStr []byte, path string) (*http.Response){
 	return resp
 }
 
-func sendTo(ids []string, cseqID uint64){
-	batch := new(Batch)
-	for _, id := range ids{
-		body := BodyWithCseqID{msgContent, cseqID}
-		msg := OutgoingMessage{id, body}
-		batch.Batch = append(batch.Batch, msg)
-	}
-	b, _ := json.Marshal(batch)
-	resp := req("POST", b, "/message")
-	defer resp.Body.Close()
-}
+// func sendTo(ids []string, cseqID uint64){
+// 	batch := new(Batch)
+// 	for _, id := range ids{
+// 		body := BodyWithCseqID{msgContent, cseqID}
+// 		msg := OutgoingMessage{id, body}
+// 		batch.Batch = append(batch.Batch, msg)
+// 	}
+// 	b, _ := json.Marshal(batch)
+// 	resp := req("POST", b, "/message")
+	// defer resp.Body.Close()
+// }
 
 func delete(seqID uint64){
 	delMsg := fmt.Sprintf(`{"seqID" : %v }`, seqID)
@@ -128,35 +125,41 @@ func main() {
 		serverAddr = os.Args[5]
 	}
 
-	keepout , _ = strconv.ParseInt(os.Args[3], 10, 0)
+	listToSend := []string{deviceId}
+	msgContent = make([]byte, msgSize)
+
+	batch := new(Batch)
+	for _, id := range listToSend{
+		body := BodyWithCseqID{msgContent, 0}
+		msg := OutgoingMessage{id, body}
+		batch.Batch = append(batch.Batch, msg)
+	}
+	msgBatch, _ := json.Marshal(batch)
+
+	// msgPostRequest, _ := http.NewRequest("POST", serverAddr + POST_PATH, bytes.NewBuffer(msgBatch))
+	// // msgPostRequest.Close = true
+	// msgPostRequest.Header = http.Header{
+	// 	"Content-Type": {"application/json"},
+	// 	"Authorization": {"Bearer " + deviceId},
+	// }
 
 	client := sse.NewClient(serverAddr + "/events")
 	client.Headers["Authorization"] = "Bearer " + deviceId
-
-	msgContent = make([]byte, msgSize)
 
 	// finish := make(chan bool)
 	httpClient = &http.Client{}
 	go client.Subscribe("msg", func(msg *sse.Event) {
 		recvCount += 1
-		
-		// if(msgType == "otkey"){
-			// var msgContent NeedsOneTimeKeyEvent
-			// json.Unmarshal([]byte(msg.Data), &msgContent)
-			// fmt.Println(msgContent.DeviceId)
-		// }
-		func(){
-			msgType := string([]byte(msg.Event))
-			if(msgType == "msg"){
-				var msgContent IncomingMessage
-				json.Unmarshal([]byte(msg.Data), &msgContent)
-				delete(msgContent.SeqID)
-			}
-		}()
+		// func(){
+		// 	msgType := string([]byte(msg.Event))
+		// 	if(msgType == "msg"){
+		// 		var msgContent IncomingMessage
+		// 		json.Unmarshal([]byte(msg.Data), &msgContent)
+		// 		delete(msgContent.SeqID)
+		// 	}
+		// }()
 	})
 	
-
-	listToSend := []string{deviceId}
 
 	var id uint64
 
@@ -175,14 +178,15 @@ func main() {
 	go func(){
 		<-timerTail.C
 		numTail = recvCount - numHead
-		fmt.Printf("%v, %v\n", deviceId, float32(numTail)/float32(duration - 2 * keepout))
+		fmt.Printf("%v, %v\n", deviceId, float32(numTail)/float32(duration - keepout))
 	}()
 
 	for id = 0; (now() - startTime <= duration * 1000000); id++ {
 		// sem <- true
 		// func(){
 			// fmt.Printf("send time[%v]: %v\n", id, now())
-		sendTo(listToSend, id)
+		// sendTo(listToSend, id)
+		req("POST", msgBatch, POST_PATH)
 			// <-sem
 		// }(i)
 	}

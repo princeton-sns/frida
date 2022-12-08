cd406c7a6f9ccf01e4221377cd76c48d83eec95c
leochanj105 <leocjy105@gmail.com>
2022-12-08 00:24:40 -0500

    fix receivers

--- experiments/receiver/run_clients_local.sh
+++ experiments/receiver/run_clients_local.sh
@@ -3,11 +3,11 @@
 nclients=$1
 name=$2
 server=$3
-
+startID=$((${4}))
 
 for (( id=0; id<$nclients; id++ ))
 do
         # taskset -c $(($id % $NPROCS))
-        ./receiver ${name}_${id} $server &
+        ./receiver ${name}_$((${id} + ${startID})) $server &
 done
 wait
